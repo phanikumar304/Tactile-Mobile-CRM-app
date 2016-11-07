bf6e490c86d82255d3b48db4fe99e0f1cd82c2aa
phanikumar304 <phanim@live.com>
2016-11-07 00:52:07 -0800

    Update EditingNotes.java

--- EditingNotes.java
+++ EditingNotes.java
@@ -1,5 +1,26 @@
-package Tact.Tactile_Optimized;
+package Tact1.Tact1_Opt;
+
+import org.openqa.selenium.By;
+
+import io.appium.java_client.android.AndroidDriver;
 
 public class EditingNotes {
+	
+	// POM Element
+	static By editNote = By.xpath("//android.widget.LinearLayout[@resource-id = 'com.tactile.tact:id/notebook_list_item_linear_layout']");
+	static By Edit = By.xpath("//android.widget.ImageView[@resource-id = 'com.tactile.tact:id/imageview_secondary_2']");
+	static By Toggle = By.xpath("//android.widget.ToggleButton[@resource-id = 'com.tactile.tact:id/sync_sf_toggle']");
+	static By Save = By.xpath("//android.widget.Button[@resource-id = 'com.tactile.tact:id/save_btn']");
+	static By Menu = By.xpath("//android.widget.ImageView[@resource-id = 'com.tactile.tact:id/fragment_menu_button']");
+	
+	//POM Method
+	public static void eNotes(AndroidDriver eNote){
+		eNote.findElement(editNote).click();
+		eNote.findElement(Edit).click();
+		eNote.findElement(Toggle).click();
+		eNote.findElement(Save).click();
+		eNote.findElement(Menu).click();
+		
+	}
 
 }
