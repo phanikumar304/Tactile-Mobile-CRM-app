42fb8ff8af88e96abe9daff6ee1769aab4ebe6ee
phanikumar304 <phanim@live.com>
2016-11-07 00:55:26 -0800

    Update TapScreen.java

--- TapScreen.java
+++ TapScreen.java
@@ -1,5 +1,27 @@
-package Tact.Tactile_Optimized;
+package Tact1.Tact1_Opt;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.TouchAction;
 
 public class TapScreen {
+	
+	//Element to tap on the screen
+	static By TapElem = By.className("android.view.View");
+
+	//Method to tap on the screen
+	public static void tapScreen(MobileDriver tapS){
+		WebDriverWait wait = new WebDriverWait(tapS, 1000); 
+	    WebElement messageElement = wait.until(ExpectedConditions.presenceOfElementLocated(TapElem));
+	    TouchAction action = new TouchAction(tapS);
+	    action.longPress(messageElement).release().perform();
+	    System.out.println("Tap got applied");
+	}
 
 }
